a9dc0ec113335dc2136e9df9122c040830c3d2ba
vladimirangarita <vladimir.angarita@gmail.com>
2022-09-18 19:32:17 -0400

    Cap 35 validaciones por modelo

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -4,25 +4,46 @@ using WebApiAutores.Validaciones;
 
 namespace WebApiAutores.Entidades
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Requerido {0}")]
         [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe tener mas de {1}  caracteres")]
-        [PrimeraLetraMayuscula]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
-        [Range(18, 120)]
+        //[Range(18, 120)]
+        //[NotMapped]
+        //public int Edad { get; set; }
+        //[CreditCard]
+        //[Required(ErrorMessage = "Requerido {0}")]
+        //[NotMapped]
+        //public string TarjetaDeCredito { get; set; }
+        //[Url]
+        //[NotMapped]
+        //public string URL { get; set; }
         [NotMapped]
-        public int Edad { get; set; }
-        [CreditCard]
-        [Required(ErrorMessage = "Requerido {0}")]
-        [NotMapped]
-        public string TarjetaDeCredito { get; set; }
-        [Url]
-        [NotMapped]
-        public string URL { get; set; }
+        public int Menor { get; set; }
+        public int Mayor { get; set; }
         public List<Libro> Libros { get; set; }
 
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera Letra debe ser mayuscula", 
+                    new string[] { nameof(Nombre) });
+                }
+            }
+            if (Menor>Mayor)
+            {
+                yield return new ValidationResult("Este valor no puede ser mas grande ue el campo mayor",
+                new string[] { nameof(Menor) });
+            }
+        }
     }
 }
